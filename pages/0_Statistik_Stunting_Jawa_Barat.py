61ca22671fe6993eb8e35592fe3da9b316e2f62c
NoerFajrin <71316603+NoerFajrin@users.noreply.github.com>
2023-10-10 23:33:33 +0000

    res

--- pages/0_Statistik_Stunting_Jawa_Barat.py
+++ pages/0_Statistik_Stunting_Jawa_Barat.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import requests
-import pandas as pd
 
 # URL API
 url = "https://data.jabarprov.go.id/api-backend/bigdata/dinkes/od_17147_jumlah_balita_stunting_berdasarkan_kabupatenkota"
@@ -9,23 +8,14 @@ url = "https://data.jabarprov.go.id/api-backend/bigdata/dinkes/od_17147_jumlah_b
 response = requests.get(url)
 
 if response.status_code == 200:
-    api_data = response.json()
-    data_stunting = api_data['data']
+    data_stunting = response.json()
 else:
     st.error("Gagal mengambil data dari API")
 
 # Judul Aplikasi
 st.title("Data Stunting di Jawa Barat")
 
-# Tampilkan Data Stunting dalam Tabel
+# Tampilkan Data Stunting
 if 'data_stunting' in locals():
-    # Buat DataFrame dari data stunting
-    df = pd.DataFrame(data_stunting, index=[0])
-
-    # Tampilkan DataFrame sebagai tabel
     st.write("Data Stunting terbaru di Jawa Barat:")
-    st.dataframe(df)
-
-# Munculkan kesalahan jika gagal mengambil data
-if 'data_stunting' not in locals():
-    st.error("Gagal mengambil data dari API")
+    st.write(data_stunting)
