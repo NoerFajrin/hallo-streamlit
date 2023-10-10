0d0134486bab0a489416a6a518534bd41908a07d
NoerFajrin <71316603+NoerFajrin@users.noreply.github.com>
2023-10-10 23:39:05 +0000

    test

--- pages/0_Statistik_Stunting_Jawa_Barat.py
+++ pages/0_Statistik_Stunting_Jawa_Barat.py
@@ -5,17 +5,14 @@ import requests
 url = "https://data.jabarprov.go.id/api-backend/bigdata/dinkes/od_17147_jumlah_balita_stunting_berdasarkan_kabupatenkota"
 
 # Mengambil data dari API
-response = requests.get(url)
+try:
+    response = requests.get(url)
 
-if response.status_code == 200:
-    data_stunting = response.json()
-else:
-    st.error("Gagal mengambil data dari API")
+    if response.status_code == 200:
+        data_stunting = response.json()
+    else:
+        st.error(f"API request failed with status code {response.status_code}")
+except Exception as e:
+    st.error(f"An error occurred while fetching data from the API: {str(e)}")
 
-# Judul Aplikasi
-st.title("Data Stunting di Jawa Barat")
-
-# Tampilkan Data Stunting
-if 'data_stunting' in locals():
-    st.write("Data Stunting terbaru di Jawa Barat:")
-    st.write(data_stunting)
+# Rest of your Streamlit code
