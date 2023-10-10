67471fac73da69fe8b82edebadd5c63deb2435cc
NoerFajrin <71316603+NoerFajrin@users.noreply.github.com>
2023-10-10 23:53:57 +0000

    aslitab

---
+++ pages/0_data_asli.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import requests
+
+# URL API
+api_url = "https://data.jabarprov.go.id/api-backend/bigdata/dinkes/od_17147_jumlah_balita_stunting_berdasarkan_kabupatenkota"
+
+# Fungsi untuk mendapatkan data dari API
+def get_api_data():
+    response = requests.get(api_url)
+    return response.json()
+
+# Tampilkan data asli dalam format JSON
+st.title("Data Balita Stunting di Jawa Barat")
+
+data = get_api_data()
+
+# Periksa apakah respon API valid
+if "data" in data:
+    st.write("Data Balita Stunting:")
+    st.json(data["data"])
+else:
+    st.write("Tidak ada data yang tersedia.")
