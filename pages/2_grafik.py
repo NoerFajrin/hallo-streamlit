9678731fdd091eb342d4551757bc74e925f95cff
NoerFajrin <71316603+NoerFajrin@users.noreply.github.com>
2023-10-11 07:11:08 +0000

    gf

--- pages/2_grafik.py
+++ pages/2_grafik.py
@@ -10,7 +10,7 @@ def get_api_data():
     response = requests.get(api_url)
     return response.json()
 
-# Tampilkan data dalam bentuk tabel
+# Tampilkan data dalam bentuk grafik
 st.title("Data Balita Stunting di Jawa Barat")
 
 data = get_api_data()
@@ -19,20 +19,32 @@ data = get_api_data()
 if "data" in data:
     st.write("Data Balita Stunting:")
 
-    # Buat daftar untuk tabel
-    table_data = []
+    # Buat daftar untuk grafik
+    kabupaten_kota = []
+    years = []
+    jumlah_stunting = []
+
     for item in data["data"]:
-        row = {
-            "Kabupaten/Kota": item['nama_kabupaten_kota'],
-            "Jumlah Balita Stunting": item['jumlah_balita_stunting'],
-            "Tahun": item['tahun'] if item['tahun'] != "Unknown Type: integer)" else "Tahun tidak valid"
-        }
-        table_data.append(row)
+        nama_kabupaten_kota = item.get('nama_kabupaten_kota', 'Kabupaten/Kota Tidak Valid')
+        try:
+            tahun = int(item['tahun'])
+        except (ValueError, TypeError):
+            tahun = 'Tahun Tidak Valid'
+        jumlah_stunting_value = item.get('jumlah_balita_stunting', 0)
 
-    # Tampilkan data dalam tabel
-    st.table(table_data)
+        kabupaten_kota.append(nama_kabupaten_kota)
+        years.append(tahun)
+        jumlah_stunting.append(jumlah_stunting_value)
 
-    # Buat grafik batang untuk setiap kota
-    st.write("Grafik Jumlah Balita Stunting per Kabupaten/Kota berdasarkan Tahun")
+    # Buat grafik batang untuk setiap kota berdasarkan tahun
+    for i, kota in enumerate(kabupaten_kota):
+        plt.figure(figsize=(10, 6))
+        plt.bar(years[i], jumlah_stunting[i])
+        plt.xlabel('Tahun')
+        plt.ylabel('Jumlah Balita Stunting')
+        plt.title(f'Grafik Jumlah Balita Stunting di {kota}')
+        st.write(f'Grafik Jumlah Balita Stunting di {kota}')
+        st.pyplot(plt)
 
-    unique_kab
+else:
+    st.write("Tidak ada data yang tersedia.")
