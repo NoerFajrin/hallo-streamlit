6f6b24ba6fc96c4102d44cff995a17e7f063d2ed
NoerFajrin <71316603+NoerFajrin@users.noreply.github.com>
2023-10-24 09:37:58 +0000

    test tets

--- pages/9_map.py
+++ pages/9_map.py
@@ -6,16 +6,22 @@ chart_data = pd.DataFrame(
     columns=['lat', 'lon']
 )
 
-# Specify the latitude and longitude of Indonesia
-indonesia_latitude = -2.4833826
-indonesia_longitude = 117.8902853
+# Specify the latitude and longitude for Aceh and Papua
+aceh_latitude = 4.2266
+aceh_longitude = 96.7494
+papua_latitude = -4.2699
+papua_longitude = 138.0804
+
+# Calculate the midpoint for the initial view
+center_latitude = (aceh_latitude + papua_latitude) / 2
+center_longitude = (aceh_longitude + papua_longitude) / 2
 
 st.pydeck_chart(pdk.Deck(
     map_style=None,
     initial_view_state=pdk.ViewState(
-        latitude=indonesia_latitude,
-        longitude=indonesia_longitude,
-        zoom=5,  # You can adjust the zoom level
+        latitude=center_latitude,
+        longitude=center_longitude,
+        zoom=4,  # Adjust the zoom level as needed
         pitch=50,
     ),
     layers=[
