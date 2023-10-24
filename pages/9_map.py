954a72919ebca4efe1aa7c94e4d40b12f4e68023
NoerFajrin <71316603+NoerFajrin@users.noreply.github.com>
2023-10-24 09:41:10 +0000

    map

--- pages/9_map.py
+++ pages/9_map.py
@@ -16,31 +16,36 @@ papua_longitude = 138.0804
 center_latitude = (aceh_latitude + papua_latitude) / 2
 center_longitude = (aceh_longitude + papua_longitude) / 2
 
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=center_latitude,
-        longitude=center_longitude,
-        zoom=4,  # Adjust the zoom level as needed
-        pitch=50,
+# Use Streamlit's layout options to set the width and height of the map
+st.write(
+    pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=center_latitude,
+            longitude=center_longitude,
+            zoom=5,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+                'HexagonLayer',
+                data=chart_data,
+                get_position='[lon, lat]',
+                radius=200,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+                pickable=True,
+                extruded=True,
+            ),
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=chart_data,
+                get_position='[lon, lat]',
+                get_color='[200, 30, 0, 160]',
+                get_radius=200,
+            ),
+        ],
     ),
-    layers=[
-        pdk.Layer(
-            'HexagonLayer',
-            data=chart_data,
-            get_position='[lon, lat]',
-            radius=200,
-            # elevation_scale=4,
-            elevation_range=[0, 1000],
-            pickable=True,
-            extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=chart_data,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-    ],
-))
+    use_container_width=True,  # Set width to the width of the Streamlit container
+    height=600  # Set a custom height (adjust as needed)
+)
