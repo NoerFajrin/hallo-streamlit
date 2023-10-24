e0549fc7deadfe4be97b4ea7c43ad5311c7e993d
NoerFajrin <71316603+NoerFajrin@users.noreply.github.com>
2023-10-24 04:36:29 +0000

    select

--- pages/1_table_stunting.py
+++ pages/1_table_stunting.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # URL API
-api_url = "https://data.jabarprov.go.id/api-backend//bigdata/dinkes/od_17147_jumlah_balita_stunting_berdasarkan_kabupatenkota?limit=300"
+api_url = "https://data.jabarprov.go.id/api-backend/bigdata/dinkes/od_17147_jumlah_balita_stunting_berdasarkan_kabupatenkota?limit=300"
 
 # Fungsi untuk mendapatkan data dari API
 def get_api_data():
@@ -18,15 +18,22 @@ data = get_api_data()
 if "data" in data:
     st.write("Data Balita Stunting:")
 
-    # Buat daftar untuk tabel
+    # Buat daftar unik tahun dari data
+    years = list(set(item['tahun'] for item in data["data"]))
+
+    # Pilih tahun menggunakan widget
+    selected_year = st.selectbox("Pilih Tahun:", years)
+
+    # Buat daftar untuk tabel berdasarkan tahun yang dipilih
     table_data = []
     for item in data["data"]:
-        row = {
-            "Kabupaten/Kota": item['nama_kabupaten_kota'],
-            "Jumlah Balita Stunting": item['jumlah_balita_stunting'],
-            "Tahun": item['tahun'] if item['tahun'] != "Unknown Type: integer)" else "Tahun tidak valid"
-        }
-        table_data.append(row)
+        if item['tahun'] == selected_year:
+            row = {
+                "Kabupaten/Kota": item['nama_kabupaten_kota'],
+                "Jumlah Balita Stunting": item['jumlah_balita_stunting'],
+                "Tahun": item['tahun']
+            }
+            table_data.append(row)
 
     # Tampilkan data dalam tabel
     st.table(table_data)
