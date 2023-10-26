b8703227130c821f9ddd94b6650778f6a3e9a44d
NoerFajrin <71316603+NoerFajrin@users.noreply.github.com>
2023-10-26 08:19:46 +0000

    map

--- pages/9_map.py
+++ pages/9_map.py
@@ -1,39 +1,42 @@
 import streamlit as st
 import pandas as pd
+import geopandas as gpd
 import pydeck as pdk
-import requests
 
-# Ambil data kabupaten/kota dari API
-api_url = "https://data.jabarprov.go.id/api-backend/bigdata/diskominfo/od_kode_wilayah_dan_nama_wilayah_kota_kabupaten"
-response = requests.get(api_url)
-data = response.json()
-kabupaten_data = data.get("data")
+# Ambil data geometri wilayah (GeoJSON) dari sumber yang sesuai
+# Misalnya, simpan dalam file seperti "wilayah_jawa_barat.geojson" dan baca dengan geopandas
+wilayah_gdf = gpd.read_file("wilayah_jawa_barat.geojson")
 
-# Buat DataFrame dari data kabupaten/kota
-df = pd.DataFrame(kabupaten_data)
+# Pastikan kolom 'luas' dalam GeoDataFrame sesuai dengan luas wilayah
+# Jika tidak ada kolom luas, Anda dapat menghitungnya dengan: wilayah_gdf['luas'] = wilayah_gdf.geometry.area
+
+# Merge data kabupaten/kota dengan data geometri wilayah
+# Pastikan ada kolom yang sesuai antara data dan data geometri (biasanya kolom 'id' atau 'kode')
+kabupaten_data = pd.read_json("https://data.jabarprov.go.id/api-backend/bigdata/diskominfo/od_kode_wilayah_dan_nama_wilayah_kota_kabupaten")
+merged_data = wilayah_gdf.merge(kabupaten_data, left_on='id', right_on='id', how='inner')
 
 # Buat peta dengan Pydeck
 st.write(
     pdk.Deck(
         map_style="mapbox://styles/mapbox/light-v9",
         initial_view_state=pdk.ViewState(
-            latitude=df["latitude"].mean(),
-            longitude=df["longitude"].mean(),
+            latitude=merged_data["latitude"].mean(),
+            longitude=merged_data["longitude"].mean(),
             zoom=7,
         ),
         layers=[
             pdk.Layer(
                 "PolygonLayer",
-                data=df,
-                get_polygon="[longitude, latitude]",
-                get_fill_color="[nilai * 2, 255 - nilai * 2, 0, 150]",
+                data=merged_data,
+                get_polygon="geometry",
+                get_fill_color="[luas * 2, 255 - luas * 2, 0, 150]",
                 get_line_color=[0, 0, 0, 255],
                 get_line_width=100,
                 pickable=True,
             ),
             pdk.Layer(
                 "ScatterplotLayer",
-                data=df,
+                data=merged_data,
                 get_position="[longitude, latitude]",
                 get_color="[255, 0, 0, 200]",
                 get_radius=200,
