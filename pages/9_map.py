925fbaded9f1490cde7c8c020c020afd2124c8ed
NoerFajrin <frinrin23@gmail.com>
2023-10-31 16:11:37 +0700

    tes

--- pages/9_map.py
+++ pages/9_map.py
@@ -1,91 +1,37 @@
 import streamlit as st
-import requests
 import pandas as pd
 import pydeck as pdk
+import requests
 
-# URL API for Balita Stunting data
-api_url_stunting = "https://data.jabarprov.go.id/api-backend/bigdata/dinkes/od_17147_jumlah_balita_stunting_berdasarkan_kabupatenkota?limit=300"
+# Mengambil data dari URL
+url = "https://data.jabarprov.go.id/api-backend/bigdata/diskominfo/od_kode_wilayah_dan_nama_wilayah_kota_kabupaten"
+response = requests.get(url)
+data = response.json()
 
-# Function to get Balita Stunting data from the API
+# Mengganti "Unknown Type: float" dengan nilai numerik yang sesuai
+for entry in data["data"]:
+    if entry["latitude"] == "Unknown Type: float":
+        entry["latitude"] = 0  # Ganti dengan nilai latitude yang sesuai
+    if entry["longitude"] == "Unknown Type: float":
+        entry["longitude"] = 0  # Ganti dengan nilai longitude yang sesuai
 
+# Mengonversi data ke dalam bentuk dataframe
+df = pd.DataFrame(data["data"])
 
-def get_api_data(api_url):
-    response = requests.get(api_url)
-    return response.json()
-
-# Function to fetch location data
-
-
-def get_location_data():
-    url = "https://data.jabarprov.go.id/api-backend/bigdata/diskominfo/od_kode_wilayah_dan_nama_wilayah_kota_kabupaten"
-    response = requests.get(url)
-    data = response.json()
-
-    # Replace "Unknown Type: float" with suitable numeric values
-    for entry in data["data"]:
-        if entry["latitude"] == "Unknown Type: float":
-            entry["latitude"] = 0  # Replace with the correct latitude value
-        if entry["longitude"] == "Unknown Type: float":
-            entry["longitude"] = 0  # Replace with the correct longitude value
-
-    return pd.DataFrame(data["data"])
-
-
-# Fetch Balita Stunting data and location data
-stunting_data = get_api_data(api_url_stunting)
-location_data = get_location_data()
-
-st.title("Tabel Data Balita Stunting di Jawa Barat")
-
-# Check if the API response is valid
-if "data" in stunting_data:
-    st.write(
-        "Berikut ini adalah Tabel Data Stunting di Provinsi Jawa Barat dari Tahun 2014 - 2021")
-
-    # Create a list of unique years from the data and sort it
-    years = sorted(list(set(item['tahun'] for item in stunting_data["data"])))
-
-    # Select a year using the widget
-    selected_year = st.selectbox("Pilih Tahun:", years)
-
-    # Create a list for the table based on the selected year
-    table_data = []
-    for item in stunting_data["data"]:
-        if item['tahun'] == selected_year:
-            row = {
-                "Kabupaten/Kota": item['nama_kabupaten_kota'],
-                "Jumlah Balita Stunting": item['jumlah_balita_stunting'],
-                "Tahun": item['tahun']
-            }
-            table_data.append(row)
-
-    # Display data in a table
-    st.table(table_data)
-else:
-    st.write("Tidak ada data yang tersedia.")
-
-# Create a map with location data and balita stunting data
-st.title("Peta Data Balita Stunting di Jawa Barat")
-
-# Merge the location data with the balita stunting data
-merged_data = pd.merge(
-    location_data, stunting_data["data"], left_on="bps_kota_nama", right_on="nama_kabupaten_kota", how="inner")
-
-# Create a map with points for each location
+# Membuat peta dengan titik di setiap latlon
 layer = pdk.Layer(
     "ScatterplotLayer",
-    data=merged_data,
+    data=df,
     get_position=["longitude", "latitude"],
-    # Use the number of stunted children to determine the radius
-    get_radius="jumlah_balita_stunting",
-    get_fill_color=[0, 255, 0],  # Green color for points
+    get_radius=1000,  # Ukuran titik
+    get_fill_color=[0, 255, 0],  # Warna titik (hijau)
     pickable=True,
 )
 
-# Create a layer for text labels
+# Membuat layer untuk teks label
 text_layer = pdk.Layer(
     "TextLayer",
-    data=merged_data,
+    data=df,
     get_position=["longitude", "latitude"],
     get_text="bps_kota_nama",
     get_size=20,
@@ -94,15 +40,15 @@ text_layer = pdk.Layer(
 )
 
 view_state = pdk.ViewState(
-    latitude=merged_data["latitude"].mean(),
-    longitude=merged_data["longitude"].mean(),
+    latitude=df["latitude"].mean(),
+    longitude=df["longitude"].mean(),
     zoom=5,
 )
 
-map_chart = pdk.Deck(
+r = pdk.Deck(
     map_style="mapbox://styles/mapbox/light-v9",
-    layers=[layer, text_layer],  # Add text layer
+    layers=[layer, text_layer],  # Tambahkan layer teks
     initial_view_state=view_state,
 )
 
-st.pydeck_chart(map_chart)
+st.pydeck_chart(r)
