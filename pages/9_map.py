0a7c8a4d2f83af88051236ad1736ac3143ba9bd7
NoerFajrin <frinrin23@gmail.com>
2023-10-31 14:23:47 +0700

    yaya

--- pages/9_map.py
+++ pages/9_map.py
@@ -3,32 +3,39 @@ import pandas as pd
 import pydeck as pdk
 import requests
 
-# URL to the GeoJSON file for Jawa Barat province
-geojson_url = "https://raw.githubusercontent.com/hitamcoklat/Jawa-Barat-Geo-JSON/master/Jabar_By_Kab.geojson"
+# Mengambil data dari URL
+url = "https://data.jabarprov.go.id/api-backend/bigdata/diskominfo/od_kode_wilayah_dan_nama_wilayah_kota_kabupaten"
+response = requests.get(url)
+data = response.json()
 
-# Data that you want to visualize (replace this with your data)
-data = pd.read_csv("data.csv")
+# Mengonversi data ke dalam bentuk dataframe
+df = pd.DataFrame(data["data"])
 
-# Create a GeoJSON layer
+# Menampilkan nama kota/kabupaten di Streamlit
+st.write("Data Nama Kota/Kabupaten:")
+st.write(df["bps_kota_nama"])
+
+# Membuat peta dengan tanda pada setiap kota/kabupaten
 layer = pdk.Layer(
-    "GeoJsonLayer",
-    data=geojson_url,
-    opacity=0.8,
-    stroked=False,
-    filled=True,
-    extruded=False,
-    get_fill_color=[128, 0, 128, 140],  # Purple color with transparency
-    get_line_color=[0, 0, 0],
-    pickable=True,
+    "TextLayer",
+    data=df,
+    get_position=["longitude", "latitude"],
+    get_text="bps_kota_nama",
+    get_color=[0, 255, 0, 255],  # Warna teks (hijau)
+    get_size=30,
+    get_alignment_baseline="bottom",
 )
 
 view_state = pdk.ViewState(
-    latitude=-6.920000,  # Replace with the appropriate latitude
-    longitude=107.600000,  # Replace with the appropriate longitude
-    zoom=7,
-    pitch=0,
+    latitude=df["latitude"].mean(),
+    longitude=df["longitude"].mean(),
+    zoom=5,
 )
 
-# Show the map using Streamlit
-st.pydeck_chart(pdk.Deck(map_style="mapbox://styles/mapbox/light-v9",
-                layers=[layer], initial_view_state=view_state))
+r = pdk.Deck(
+    map_style="mapbox://styles/mapbox/light-v9",
+    layers=[layer],
+    initial_view_state=view_state,
+)
+
+st.pydeck_chart(r)
