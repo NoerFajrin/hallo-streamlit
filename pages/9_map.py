b70e94e7c436a7399bfe0bd44b6259de5f8d75b6
NoerFajrin <frinrin23@gmail.com>
2023-10-31 16:32:37 +0700

    tetes

--- pages/9_map.py
+++ pages/9_map.py
@@ -3,35 +3,51 @@ import pandas as pd
 import pydeck as pdk
 import requests
 
-# Mengambil data dari URL
-url = "https://data.jabarprov.go.id/api-backend/bigdata/diskominfo/od_kode_wilayah_dan_nama_wilayah_kota_kabupaten"
-response = requests.get(url)
-data = response.json()
+# Fetch location data
+url_location = "https://data.jabarprov.go.id/api-backend/bigdata/diskominfo/od_kode_wilayah_dan_nama_wilayah_kota_kabupaten"
+response_location = requests.get(url_location)
+data_location = response_location.json()
 
-# Mengganti "Unknown Type: float" dengan nilai numerik yang sesuai
-for entry in data["data"]:
+# Convert location data to a DataFrame
+df_location = pd.DataFrame(data_location["data"])
+
+# Replace "Unknown Type: float" with suitable numeric values for latitude and longitude
+for entry in df_location:
     if entry["latitude"] == "Unknown Type: float":
-        entry["latitude"] = 0  # Ganti dengan nilai latitude yang sesuai
+        entry["latitude"] = 0  # Replace with the correct latitude value
     if entry["longitude"] == "Unknown Type: float":
-        entry["longitude"] = 0  # Ganti dengan nilai longitude yang sesuai
+        entry["longitude"] = 0  # Replace with the correct longitude value
 
-# Mengonversi data ke dalam bentuk dataframe
-df = pd.DataFrame(data["data"])
+# Fetch additional data with "jumlah_balita_stunting"
+url_stunting = "https://data.jabarprov.go.id/api-backend/bigdata/dinkes/od_17147_jumlah_balita_stunting_berdasarkan_kabupatenkota?limit=300"
+response_stunting = requests.get(url_stunting)
+data_stunting = response_stunting.json()
 
-# Membuat peta dengan titik di setiap latlon
+# Convert the additional data to a DataFrame
+df_stunting = pd.DataFrame(data_stunting["data"])
+
+# Filter the additional data for the "tahun" you want (replace 'year_value' with the actual year)
+year_value = "2023"  # Replace with the desired year
+df_stunting_filtered = df_stunting[df_stunting["tahun"] == year_value]
+
+# Merge the location data with the filtered additional data using "nama_kabupaten_kota" as the common key
+merged_data = df_location.merge(
+    df_stunting_filtered, left_on="bps_kota_nama", right_on="nama_kabupaten_kota", how="left")
+
+# Create a map with points for each location
 layer = pdk.Layer(
     "ScatterplotLayer",
-    data=df,
+    data=merged_data,
     get_position=["longitude", "latitude"],
-    get_radius=1000,  # Ukuran titik
-    get_fill_color=[0, 255, 0],  # Warna titik (hijau)
+    get_radius=1000,  # Point size
+    get_fill_color=[0, 255, 0],  # Green color for points
     pickable=True,
 )
 
-# Membuat layer untuk teks label
+# Create a layer for text labels
 text_layer = pdk.Layer(
     "TextLayer",
-    data=df,
+    data=merged_data,
     get_position=["longitude", "latitude"],
     get_text="bps_kota_nama",
     get_size=20,
@@ -40,15 +56,17 @@ text_layer = pdk.Layer(
 )
 
 view_state = pdk.ViewState(
-    latitude=df["latitude"].mean(),
-    longitude=df["longitude"].mean(),
+    latitude=merged_data["latitude"].mean(),
+    longitude=merged_data["longitude"].mean(),
     zoom=5,
 )
 
 r = pdk.Deck(
     map_style="mapbox://styles/mapbox/light-v9",
-    layers=[layer, text_layer],  # Tambahkan layer teks
+    layers=[layer, text_layer],
     initial_view_state=view_state,
 )
+
+st.title("Peta Data Balita Stunting di Jawa Barat")
 
 st.pydeck_chart(r)
