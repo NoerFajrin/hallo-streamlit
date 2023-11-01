b65001adf7f00f434fd74083a66f4ad468e2e431
NoerFajrin <frinrin23@gmail.com>
2023-11-01 07:16:25 +0700

    b

--- pages/9_map.py
+++ pages/9_map.py
@@ -33,31 +33,18 @@ data = response.json().get('data', [])
 # Membuat DataFrame dengan data kota/kabupaten
 df = pd.DataFrame(data)
 
-# Ambil latitude, longitude, dan nama kota/kabupaten dari DataFrame
+# Ambil latitude dan longitude dari DataFrame
 latitudes = df['latitude'].astype(float)
 longitudes = df['longitude'].astype(float)
-kota_nama = df['bps_kota_nama'].astype('string')
 
-# Buat DataFrame dengan data latitude, longitude, dan nama kota/kabupaten
-chart_data = pd.DataFrame(
-    {'lat': latitudes, 'lon': longitudes, 'kota_nama': kota_nama})
+# Buat DataFrame dengan data latitude dan longitude
+chart_data = pd.DataFrame({'lat': latitudes, 'lon': longitudes})
 
 # Set initial view untuk fokus ke Jawa Barat
 center_latitude = -6.920434
 center_longitude = 107.604953
 
-# Tambahkan ikon marker dengan teks nama kota/kabupaten
-icon_layer = pdk.Layer(
-    'IconLayer',
-    data=chart_data,
-    get_position='[lon, lat]',
-    get_color=[0, 0, 255, 160],  # Warna biru
-    get_text='kota_nama',  # Menampilkan teks nama kota/kabupaten
-    get_text_size=20,  # Ukuran teks
-    get_text_color=[0, 0, 0, 255],  # Warna teks
-)
-
-# Buat peta dengan ikon marker dan teks di setiap kota/kabupaten
+# Buat peta dengan tanda biru di semua kota/kabupaten
 st.write(
     pdk.Deck(
         map_style='mapbox://styles/mapbox/light-v9',
@@ -67,7 +54,17 @@ st.write(
             zoom=8,
             pitch=50,
         ),
-        layers=[icon_layer],
+        layers=[
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=chart_data,
+                get_position='[lon, lat]',
+                get_icon='marker',  # Nama ikon marker
+                get_size=5,  # Ukuran ikon marker
+                get_color='[0, 0, 255, 160]',  # Warna biru
+                get_radius=200,
+            ),
+        ],
     ),
     use_container_width=True,
     height=800
