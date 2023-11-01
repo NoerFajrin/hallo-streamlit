3d79dcfa98cf1c579a7533156ee7396409e15a20
NoerFajrin <frinrin23@gmail.com>
2023-11-01 08:33:21 +0700

    ss

--- pages/10_dummy.py
+++ pages/10_dummy.py
@@ -2,74 +2,32 @@ import streamlit as st
 import pydeck as pdk
 import pandas as pd
 
-# Define view state for the map
-view_state = pdk.ViewState(
-    latitude=32,
-    longitude=35,
-    zoom=7,
-    pitch=0
-)
+# Create a DataFrame with dummy data
+data = pd.DataFrame({
+    'latitude': [37.7749],
+    'longitude': [-122.4194],
+    'text': ['My home']
+})
 
-# Sample data
-data = {
-    'lon': [35, 35.1],
-    'lat': [32.5, 32.6],
-    'name': ['meA', 'meB'],
-    'prec': [100, 300],
-    'temp': [10, 30],
-    'elevationValue': [100, 300]
-}
+# Create a Pydeck map
+st.pydeck_chart(pdk.Deck(
+    map_style='mapbox://styles/mapbox/light-v9',
+    initial_view_state=pdk.ViewState(
+        latitude=data['latitude'].mean(),
+        longitude=data['longitude'].mean(),
+        zoom=10,
+        pitch=0,
+    ),
+    layers=[
+        pdk.Layer(
+            'TextLayer',
+            data=data,
+            get_position='[longitude, latitude]',
+            get_text='text',
+            get_size=16,
+            get_color=[255, 0, 0],
+        ),
+    ],
+))
 
-# Create a DataFrame
-df = pd.DataFrame(data)
-
-# Define the tooltip for data points
-tooltip = {
-    "html": "Name: {name}\nRain: {prec} mm",
-    "style": {
-        "backgroundColor": "steelblue",
-        "color": "black"
-    }
-}
-
-# Create a ScatterplotLayer
-scatterplot_layer = pdk.Layer(
-    type='ScatterplotLayer',
-    data=df,
-    get_position=["lon", "lat"],
-    get_color=["255-temp", "31+temp", "31+temp*3"],
-    get_line_color=[0, 0, 0],
-    get_radius=1750,
-    pickable=True,
-    onClick=True,
-    filled=True,
-    line_width_min_pixels=10,
-    opacity=2
-)
-
-# Create a TextLayer for displaying labels
-text_layer = pdk.Layer(
-    "TextLayer",
-    df,
-    pickable=False,
-    get_position=["lon", "lat"],
-    get_text="name",
-    get_size=3000,
-    sizeUnits='meters',
-    get_color=[0, 0, 0],
-    get_angle=0,
-    getTextAnchor="middle",
-    get_alignment_baseline="center"
-)
-
-# Create the PyDeck Deck
-deck = pdk.Deck(
-    initial_view_state=view_state,
-    map_provider='mapbox',
-    map_style=pdk.map_styles.SATELLITE,
-    layers=[scatterplot_layer, text_layer],
-    tooltip=tooltip
-)
-
-# Display the map in Streamlit
-st.pydeck_chart(deck)
+st.write("Text added to the map at (latitude, longitude): 37.7749, -122.4194")
