daab8f640a17c22a0f1a48873b91c7a5f514698a
NoerFajrin <frinrin23@gmail.com>
2023-11-01 12:35:46 +0700

    k

--- pages/10_dummy.py
+++ pages/10_dummy.py
@@ -1,38 +1,59 @@
+import pandas as pd
 import streamlit as st
 import pydeck as pdk
-import pandas as pd
 
-# Create a DataFrame with dummy data for multiple points
 data = pd.DataFrame({
     'latitude': [37.7749, 37.785, 37.793],
     'longitude': [-122.4194, -122.395, -122.408],
     'text': ['Point 1', 'Point 2', 'Point 3'],
-    # Red, Blue, Green colors for text
     'text_color': [[255, 0, 0], [0, 0, 255], [0, 255, 0]],
-    # Green, Red, Blue circles (with alpha value for transparency)
     'circle_color': [[0, 255, 0, 150], [255, 0, 0, 150], [0, 0, 255, 150]]
 })
 
-# Create a Pydeck map
-st.pydeck_chart(pdk.Deck(
-    map_style='mapbox://styles/mapbox/light-v9',
-    initial_view_state=pdk.ViewState(
-        latitude=data['latitude'].mean(),
-        longitude=data['longitude'].mean(),
-        zoom=10,
-        pitch=0,
-    ),
-    layers=[
-        pdk.Layer(
-            'TextLayer',
-            data=data,
-            get_position='[longitude, latitude]',
-            get_text='text',
-            get_size=16,
-            get_color='text_color',
-            get_background_color='circle_color',
-        ),
-    ],
-))
+st.header("My PyDeck Map")
 
-st.write("Multiple points added to the map.")
+# Layer pertama untuk warna lokasi
+layer_location = pdk.Layer(
+    "ScatterplotLayer",
+    data=data,
+    get_position=["longitude", "latitude"],
+    get_fill_color="circle_color",
+    get_radius=100,
+    pickable=True,
+    auto_highlight=True,
+)
+
+# Layer kedua untuk nomor
+data['number'] = [1, 2, 3]
+layer_number = pdk.Layer(
+    "TextLayer",
+    data=data,
+    get_position=["longitude", "latitude"],
+    get_text="number",
+    get_color=[0, 0, 0],
+    get_size=25,
+    get_alignment_baseline="'bottom'",
+)
+
+# Layer ketiga untuk dot color
+layer_dot_color = pdk.Layer(
+    "ScatterplotLayer",
+    data=data,
+    get_position=["longitude", "latitude"],
+    get_fill_color="text_color",
+    get_radius=100,
+    pickable=True,
+    auto_highlight=True,
+)
+
+# Membuat peta
+view_state = pdk.ViewState(
+    latitude=data['latitude'].mean(),
+    longitude=data['longitude'].mean(),
+    zoom=8,
+)
+r = pdk.Deck(layers=[layer_location, layer_number,
+             layer_dot_color], initial_view_state=view_state)
+
+# Menampilkan peta menggunakan komponen Streamlit
+st.pydeck_chart(r)
