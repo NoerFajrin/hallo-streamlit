e53a6daaccb23fbb26b254772bdf746c0e9dd863
NoerFajrin <frinrin23@gmail.com>
2023-11-01 08:26:49 +0700

    ss

--- pages/10_dummy.py
+++ pages/10_dummy.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pydeck as pdk
-import pandas as pd  # Add this import statement
+import pandas as pd
 
 # Sample data with latitude, longitude, and labels
 data = [
-    {'lat': 37.7749, 'lon': -122.4194, 'name': 'RinRIn'},
+    {'lat': 37.7749, 'lon': -122.4194, 'name': 'RinRin'},
     {'lat': 34.0522, 'lon': -118.2437, 'name': 'Juna'},
     {'lat': 40.7128, 'lon': -74.0060, 'name': 'New York'},
 ]
@@ -12,14 +12,14 @@ data = [
 # Create a DataFrame from the sample data
 df = pd.DataFrame(data)
 
-# Create a map centered on the United States
+# Create a map centered on the United States with a higher zoom level
 st.write(
     pdk.Deck(
         map_style='mapbox://styles/mapbox/light-v9',
         initial_view_state=pdk.ViewState(
             latitude=39.8097,
             longitude=-98.5556,
-            zoom=4,
+            zoom=5,  # Adjust the zoom level as needed
             pitch=50,
         ),
         layers=[
