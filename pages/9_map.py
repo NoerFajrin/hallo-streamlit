6b265fcbdefa2b87f44b25d9f9e9a656c789aef6
NoerFajrin <frinrin23@gmail.com>
2023-11-01 07:36:26 +0700

    s

--- pages/9_map.py
+++ pages/9_map.py
@@ -1,13 +1,23 @@
 import streamlit as st
-import pandas as pd
-import pydeck as pdk
 import requests
 
-# Endpoint untuk data kota/kabupaten di Provinsi Jawa Barat
-endpoint = 'https://data.jabarprov.go.id/api-backend/bigdata/diskominfo/od_kode_wilayah_dan_nama_wilayah_kota_kabupaten'
+# Define the API endpoint
+api_endpoint = 'https://jsonplaceholder.typicode.com/posts/1'
 
-# Mengambil data dari endpoint
-response = requests.get(endpoint)
-st.write("Response Status Code:", response.status_code)
-data = response.json().get('data', [])
-st.write(data)
+# Make a GET request to the API
+response = requests.get(api_endpoint)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse the JSON response
+    data = response.json()
+
+    # Display the data in the Streamlit app
+    st.title('API Data Display')
+    st.write('Data from the API:')
+    st.write(data)
+else:
+    st.error(
+        f'Failed to retrieve data from the API. Status code: {response.status_code}')
+
+# You can add more Streamlit components to format the output as needed
