df22d53dd820a8553ad762d5164034d081db6146
NoerFajrin <frinrin23@gmail.com>
2023-11-01 07:28:38 +0700

    hehe

--- pages/9_map.py
+++ pages/9_map.py
@@ -5,11 +5,10 @@ import requests
 
 # Endpoint untuk data kota/kabupaten di Provinsi Jawa Barat
 endpoint = 'https://data.jabarprov.go.id/api-backend/bigdata/diskominfo/od_kode_wilayah_dan_nama_wilayah_kota_kabupaten'
+
 # Mengambil data dari endpoint
 response = requests.get(endpoint)
 data = response.json().get('data', [])
-st.write(data)
-
 
 # Membuat DataFrame dengan data kota/kabupaten
 df = pd.DataFrame(data)
@@ -17,6 +16,7 @@ df = pd.DataFrame(data)
 # Ambil latitude dan longitude dari DataFrame
 latitudes = df['latitude'].astype(float)
 longitudes = df['longitude'].astype(float)
+kota_nama = df['bps_kota_nama']
 
 # Buat DataFrame dengan data latitude dan longitude
 chart_data = pd.DataFrame({'lat': latitudes, 'lon': longitudes})
@@ -25,7 +25,26 @@ chart_data = pd.DataFrame({'lat': latitudes, 'lon': longitudes})
 center_latitude = -6.920434
 center_longitude = 107.604953
 
-# Buat peta dengan tanda biru di semua kota/kabupaten
+# Tambahkan layer untuk warna (misalnya, marker)
+marker_layer = pdk.Layer(
+    'ScatterplotLayer',
+    data=chart_data,
+    get_position='[lon, lat]',
+    get_color=[0, 0, 255, 160],  # Warna biru
+    get_radius=200,
+)
+
+# Tambahkan layer untuk teks (nama kota/kabupaten)
+text_layer = pdk.Layer(
+    'TextLayer',
+    data=chart_data,
+    get_position='[lon, lat]',
+    get_text='kota_nama',  # Menampilkan teks nama kota/kabupaten
+    get_size=20,  # Ukuran teks
+    get_color=[0, 0, 0, 255],  # Warna teks
+)
+
+# Buat peta dengan dua layer terpisah
 st.write(
     pdk.Deck(
         map_style='mapbox://styles/mapbox/light-v9',
@@ -35,22 +54,7 @@ st.write(
             zoom=8,
             pitch=50,
         ),
-        layers=[
-            pdk.Layer(
-                'ScatterplotLayer',
-                data=chart_data,
-                get_position='[lon, lat]',
-                get_color='[0, 0, 255, 160]',  # Warna biru
-                get_radius=200,
-            ),
-            pdk.Layer(
-                'ScatterplotLayer',
-                data=chart_data,
-                get_position='[lon, lat]',
-                get_text="halo",  # Warna biru
-                # get_radius=200,
-            ),
-        ],
+        layers=[marker_layer, text_layer],
     ),
     use_container_width=True,
     height=800
