d08beb2b70b926a221195b302f624e45508091d2
NoerFajrin <frinrin23@gmail.com>
2023-11-02 22:27:56 +0700

    ee

--- pages/11_maptinggi.py
+++ pages/11_maptinggi.py
@@ -1,34 +1,40 @@
 import pydeck as pdk
 import streamlit as st
 
-UK_ACCIDENTS_DATA = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv'
+# Create a DataFrame from the provided coordinates
+import pandas as pd
 
+data = {
+    'lng': [-0.198465, -0.178838, -0.205590, -0.208327],
+    'lat': [51.505538, 51.491836, 51.514910, 51.514952]
+}
+
+df = pd.DataFrame(data)
+
+# Create a PyDeck layer using the provided data
 layer = pdk.Layer(
     'HexagonLayer',
-    UK_ACCIDENTS_DATA,
+    df,
     get_position=['lng', 'lat'],
     auto_highlight=True,
     elevation_scale=50,
     pickable=True,
     elevation_range=[0, 3000],
     extruded=True,
-    coverage=1)
+    coverage=1
+)
 
 view_state = pdk.ViewState(
-    longitude=-1.415,
-    latitude=52.2323,
-    zoom=6,
+    longitude=-0.2,
+    latitude=51.5,
+    zoom=12,
     min_zoom=5,
     max_zoom=15,
     pitch=40.5,
-    bearing=-27.36)
+    bearing=-27.36
+)
 
-st.title("3D Hexagon Heatmap Example")
+st.title("Custom 3D Hexagon Heatmap Example")
 
 # Display the PyDeck map using st.pydeck_chart
 st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state))
-
-# Optionally, you can save the HTML output for the visualization
-st.markdown("You can also save the visualization as an HTML file:")
-st.markdown(
-    "[Download hexagon-example.html](sandbox:/path/to/hexagon-example.html)")
