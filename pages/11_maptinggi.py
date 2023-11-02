6d2a76729b14db2ed4f827769326224113376335
NoerFajrin <frinrin23@gmail.com>
2023-11-02 22:34:40 +0700

    s

--- pages/11_maptinggi.py
+++ pages/11_maptinggi.py
@@ -1,40 +1,45 @@
+import numpy as np
+import pandas as pd
 import pydeck as pdk
+
 import streamlit as st
 
-# Create a DataFrame from the provided coordinates
-import pandas as pd
-
-data = {
-    'lng': [-0.198465, -0.178838, -0.205590, -0.208327],
-    'lat': [51.505538, 51.491836, 51.514910, 51.514952]
-}
-
-df = pd.DataFrame(data)
-
-# Create a PyDeck layer using the provided data
-layer = pdk.Layer(
-    'HexagonLayer',
-    df,
-    get_position=['lng', 'lat'],
-    auto_highlight=True,
-    elevation_scale=50,
-    pickable=True,
-    elevation_range=[0, 3000],
-    extruded=True,
-    coverage=1
+chart_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4], columns=["lat", "lon"]
 )
 
-view_state = pdk.ViewState(
-    longitude=-0.2,
-    latitude=51.5,
-    zoom=12,
-    min_zoom=5,
-    max_zoom=15,
-    pitch=40.5,
-    bearing=-27.36
+
+height = st.slider("Height", 100, 1000, 500)
+
+chart = pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+            "HexagonLayer",
+            data=chart_data,
+            get_position="[lon, lat]",
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+        ),
+        pdk.Layer(
+            "ScatterplotLayer",
+            data=chart_data,
+            get_position="[lon, lat]",
+            get_color="[200, 30, 0, 160]",
+            get_radius=200,
+        ),
+    ],
+    height=height,  # Set the height of the map
 )
 
-st.title("Custom 3D Hexagon Heatmap Example")
-
-# Display the PyDeck map using st.pydeck_chart
-st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state))
+# Pass the height to the frame
+st.components.v1.html(chart.to_html(as_string=True), height=height)
