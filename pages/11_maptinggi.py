2bbc58fb96d08b7090cf4703fa0bf12252f09390
NoerFajrin <frinrin23@gmail.com>
2023-11-02 23:04:10 +0700

    s

--- pages/11_maptinggi.py
+++ pages/11_maptinggi.py
@@ -1,97 +1,38 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import pydeck as pdk
-import requests
 
-# Define the API endpoints
-endpoint_data_stunting = "https://data.jabarprov.go.id/api-backend/bigdata/dinkes/od_17147_jumlah_balita_stunting_berdasarkan_kabupatenkota?limit=300"
-endpoint_data_lat_lon = 'https://data.jabarprov.go.id/api-backend/bigdata/diskominfo/od_kode_wilayah_dan_nama_wilayah_kota_kabupaten'
+chart_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+st.write(chart_data)
 
-# Make GET requests to the APIs
-response = requests.get(endpoint_data_lat_lon)
-responseStunting = requests.get(endpoint_data_stunting)
-
-# Parse the JSON responses
-data_lat_lon = response.json().get('data', [])
-data_stunting = responseStunting.json().get('data', [])
-
-# Create a list of years for selection
-years = sorted(list(set(stunting_data["tahun"]
-               for stunting_data in data_stunting)))
-
-# Allow the user to select a year
-selected_year = st.selectbox("Pilih Tahun:", years)
-
-# Combine the data for the selected year
-combined_data = []
-
-for stunting_data in data_stunting:
-    nama_kabupaten_kota_stunting = stunting_data["nama_kabupaten_kota"]
-    jumlah_balita_stunting = int(
-        stunting_data["jumlah_balita_stunting"])  # Convert to int
-    tahun = stunting_data["tahun"]
-
-    # Filter data for the selected year
-    if tahun == selected_year:
-        # Find matching data in data_lat_lon based on the name of the kabupaten/kota
-        matching_lat_lon_data = [
-            item for item in data_lat_lon if item["bps_kota_nama"] == nama_kabupaten_kota_stunting]
-
-        if matching_lat_lon_data:
-            lat = matching_lat_lon_data[0]["latitude"]
-            lon = matching_lat_lon_data[0]["longitude"]
-
-            # Create a new data object
-            data_baru = {
-                "nama_kab": nama_kabupaten_kota_stunting,
-                "lat": lat,
-                "lon": lon,
-                "balita_stunting": jumlah_balita_stunting,
-                "tahun": tahun
-            }
-
-            combined_data.append(data_baru)
-
-# Create a DataFrame
-df = pd.DataFrame(combined_data)
-
-# Filter DataFrame for the selected year
-filtered_data = df[df['tahun'] == selected_year]
-# Convert the 'tahun' and 'balita_stunting' columns to strings
-filtered_data['tahun'] = filtered_data['tahun'].astype(str)
-filtered_data['balita_stunting'] = filtered_data['balita_stunting'].astype(
-    int)  # Ensure it's integer
-
-# Create a PyDeck map with hexagonal layer and text labels
-view_state = pdk.ViewState(
-    latitude=filtered_data['lat'].mean(),
-    longitude=filtered_data['lon'].mean(),
-    zoom=9,
-    pitch=50,
-)
-
-# Create a hexagonal layer using 'balita_stunting' as elevation
-hexagon_layer = pdk.Layer(
-    "HexagonLayer",
-    data=filtered_data,
-    get_position='[lon, lat]',
-    radius=1000,  # Set your desired radius for the hexagons
-    elevation_scale=20,  # You can adjust this value as needed
-    elevation_range=[0, 2000],  # Set your desired elevation range
-    # Use 'balita_stunting' column for elevation
-    pickable=True,
-    extruded=True,
-)
-
-# Create a PyDeck Deck with the hexagonal layer
-deck = pdk.Deck(
-    map_style='mapbox://styles/mapbox/light-v9',
-    initial_view_state=view_state,
-    layers=[hexagon_layer],
-)
-
-# Display the PyDeck map
-st.pydeck_chart(deck)
-
-# Display the DataFrame
-st.write(filtered_data)
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+            'HexagonLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
