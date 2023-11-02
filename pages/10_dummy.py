d1abb016b626e0d2679e075036ae5a67f6ed0a6f
NoerFajrin <frinrin23@gmail.com>
2023-11-02 14:44:17 +0700

    tes

--- pages/10_dummy.py
+++ pages/10_dummy.py
@@ -87,7 +87,7 @@ st.pydeck_chart(pdk.Deck(
             'TextLayer',
             data=filtered_data,
             get_position='[lon, lat]',
-            get_text='nama_kab',
+            get_text='tahun',
             get_size=18,  # Text label size
             get_color='[0, 0, 0, 255]',  # Text label color (black)
         ),
