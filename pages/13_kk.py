a6bd9c017798bf84a1dcb83bd292f53ca14afb21
NoerFajrin <frinrin23@gmail.com>
2023-11-03 22:36:08 +0700

    s

--- pages/13_kk.py
+++ pages/13_kk.py
@@ -9,19 +9,13 @@ if datadunia is not None:
     # Replace ',' with '.' in all columns of the DataFrame
     datadunia = datadunia.replace(',', '.', regex=True)
 
-    # Print the DataFrame
-    st.write(datadunia)
-    df = pd.DataFrame(datadunia.to_json(orient='records'))
     # Mengekstrak Longitude (lon) dan Latitude (lat) dari "Data Awal"
-    df['Longitude'] = df['Data Awal'].str.extract(
+    datadunia['Longitude'] = datadunia['Data Awal'].str.extract(
         r'Longitude \(lon\) = ([\d.-]+)')
-    df['Latitude'] = df['Data Awal'].str.extract(
+    datadunia['Latitude'] = datadunia['Data Awal'].str.extract(
         r'Latitude \(lat\) = ([\d.-]+)')
 
-    # Menampilkan DataFrame baru
-    print(df)
-
-    # Convert DataFrame to JSON and display it
-    # st.json(datadunia.to_json(orient='records'))
+    # Print the updated DataFrame
+    st.write(datadunia)
 else:
     st.write("Data not found or could not be loaded.")
