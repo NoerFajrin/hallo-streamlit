a5d5601b92fa52dd65ebba5d8043969a38d15ae5
NoerFajrin <frinrin23@gmail.com>
2023-11-03 22:19:46 +0700

    s

--- pages/13_kk.py
+++ pages/13_kk.py
@@ -6,7 +6,11 @@ datadunia = pd.read_csv('datadunia.csv')
 
 # Check if the data has been loaded
 if datadunia is not None:
-    # Print the DataFrame
+    # Split the 'data awal' column into 'Longitude (lon)' and 'Latitude (lat)'
+    datadunia[['Longitude (lon)', 'Latitude (lat)']] = datadunia['data awal'].str.extract(
+        r'Longitude \(lon\) = ([^;]+); Latitude \(lat\) = ([^)]+)')
+
+    # Print the updated DataFrame
     st.write(datadunia)
 else:
     st.write("Data not found or could not be loaded.")
