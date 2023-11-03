42989f2bcb7688570f48152fc4c7c23f54f65f32
NoerFajrin <frinrin23@gmail.com>
2023-11-03 07:48:04 +0700

    k

--- pages/12_dd.py
+++ pages/12_dd.py
@@ -1,36 +1,37 @@
 import streamlit as st
-import pydeck as pdk
 import pandas as pd
+import numpy as np
+import pydeck as pdk
 
-CPU_GRID_LAYER_DATA = (
-    "https://raw.githubusercontent.com/uber-common/"
-    "deck.gl-data/master/website/sf-bike-parking.json"
-)
-
-st.title("GridLayer Example")
-
-# Load the data
-df = pd.read_json(CPU_GRID_LAYER_DATA)
-
-# Define a layer to display on a map
-layer = pdk.Layer(
-    "GridLayer",
-    df,
-    pickable=True,
-    extruded=True,
-    cell_size=200,
-    elevation_scale=4,
-    get_position="COORDINATES",
-)
-
-view_state = pdk.ViewState(
-    latitude=37.7749295,
-    longitude=-122.4194155,
-    zoom=11,
-    bearing=0,
-    pitch=45,
-)
-
-# Render the PyDeck map using Streamlit
-st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state, tooltip={
-                "text": "{position}\nCount: {count}"}))
+chart_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+st.write(chart_data)
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+            'HexagonLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
