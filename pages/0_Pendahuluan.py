b5c3d90eb8545b287385ab90b28c42064cf62469
NoerFajrin <frinrin23@gmail.com>
2023-11-05 01:38:02 +0700

    s

--- pages/0_Pendahuluan.py
+++ pages/0_Pendahuluan.py
@@ -14,32 +14,18 @@ if datadunia is not None:
         r'Longitude \(lon\) = ([\d.-]+)')
     datadunia['Latitude'] = datadunia['Data Awal'].str.extract(
         r'Latitude \(lat\) = ([\d.-]+)')
-
-    # Print the updated DataFrame
-    st.write(datadunia)
-
     # Filter the years (2000-2022)
     selected_years = st.selectbox("Select Year", list(range(2000, 2023)))
-
     # Select only the relevant columns
     selected_cols = ["Country and areas", str(
         selected_years), "Latitude", "Longitude"]
-
     # Filter and sort the DataFrame
     sorted_df = datadunia[selected_cols].sort_values(
         by=str(selected_years), ascending=False)
-
     # Reset the index to start from 1 for the first row
     sorted_df = sorted_df.reset_index(drop=True)
     sorted_df.index += 1  # Start the index from 1
-
-    # Display the sorted DataFrame with the modified index
+    # Display the sorsted DataFrame with the modified index
     st.write(sorted_df)
-
-    # Convert the DataFrame to a JSON representation
-    json_representation = sorted_df.json().get('data', [])
-
-    # Display the JSON representation using st.write
-    st.write(json_representation)
 else:
     st.write("Data not found or could not be loaded.")
