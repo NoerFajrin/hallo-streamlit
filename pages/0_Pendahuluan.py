b128c6697ce38daa6b07c632ccb8fc5ee21158e4
NoerFajrin <frinrin23@gmail.com>
2023-11-05 01:33:47 +0700

    x

--- pages/0_Pendahuluan.py
+++ pages/0_Pendahuluan.py
@@ -15,22 +15,31 @@ if datadunia is not None:
     datadunia['Latitude'] = datadunia['Data Awal'].str.extract(
         r'Latitude \(lat\) = ([\d.-]+)')
 
-    # Filter the years (2000-2022)
-    selected_year_label = "Pilih Tahun"
-    selected_year = st.selectbox(selected_year_label, list(range(2000, 2023)))
+    # Print the updated DataFrame
+    st.write(datadunia)
 
-    # Rename the columns in the DataFrame
-    datadunia = datadunia.rename(columns={
-        "Country and areas": "Negara",
-        str(selected_year): "Estimate",
-        "Latitude": "lat",
-        "Longitude": "lon"
-    })
+    # Filter the years (2000-2022)
+    selected_years = st.selectbox("Select Year", list(range(2000, 2023)))
 
     # Select only the relevant columns
-    selected_cols = ["Negara", "Estimate", "lat", "lon"]
+    selected_cols = ["Country and areas", str(
+        selected_years), "Latitude", "Longitude"]
 
-    # Display the sorted DataFrame with the modified column names and the modified index
-    st.write(datadunia)
+    # Filter and sort the DataFrame
+    sorted_df = datadunia[selected_cols].sort_values(
+        by=str(selected_years), ascending=False)
+
+    # Reset the index to start from 1 for the first row
+    sorted_df = sorted_df.reset_index(drop=True)
+    sorted_df.index += 1  # Start the index from 1
+
+    # Display the sorted DataFrame with the modified index
+    st.write(sorted_df)
+
+    # Convert the DataFrame to a JSON representation
+    json_representation = sorted_df.to_json(orient='records')
+
+    # Display the JSON representation using st.write
+    st.write(json_representation)
 else:
     st.write("Data not found or could not be loaded.")
