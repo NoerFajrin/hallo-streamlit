8d9a49f9e18799a1110080f6f6eb2be69a02a4e8
NoerFajrin <frinrin23@gmail.com>
2023-11-05 01:56:35 +0700

    s

--- pages/0_Pendahuluan.py
+++ pages/0_Pendahuluan.py
@@ -9,28 +9,22 @@ if datadunia is not None:
     # Replace ',' with '.' in all columns of the DataFrame
     datadunia = datadunia.replace(',', '.', regex=True)
 
-    # Extract Longitude (lon) and Latitude (lat) from "Data Awal"
+    # Extract Longitude (lon) and Latitude (lat) from "Data Awal" and convert to float
     datadunia['Longitude'] = datadunia['Data Awal'].str.extract(
-        r'Longitude \(lon\) = ([\d.-]+)').astype(float)
+        r'Longitude \(lon\) = ([\d.-]+)')
     datadunia['Latitude'] = datadunia['Data Awal'].str.extract(
-        r'Latitude \(lat\) = ([\d.-]+)').astype(float)
+        r'Latitude \(lat\) = ([\d.-]+)')
+
+    try:
+        datadunia['Longitude'] = datadunia['Longitude'].astype(float)
+        datadunia['Latitude'] = datadunia['Latitude'].astype(float)
+    except ValueError as e:
+        st.error(f"Error converting to float: {e}")
+
     # Filter the years (2000-2022)
     selected_years = st.selectbox("Select Year", list(range(2000, 2023)))
-    # Select only the relevant columns
-    selected_cols = ["Country and areas", str(
-        selected_years), "Latitude", "Longitude"]
-    # Filter and sort the DataFrame
-    sorted_df = datadunia[selected_cols].sort_values(
-        by=str(selected_years), ascending=False)
-    # Reset the index to start from 1 for the first row
-    sorted_df = sorted_df.reset_index(drop=True)
-    sorted_df.index += 1  # Start the index from 1
-    # Display the sorsted DataFrame with the modified index
-    st.write(sorted_df)
 
-    json_array = sorted_df.to_json(orient='records')
+    # ... Rest of your code for sorting and displaying
 
-    # Display the JSON array
-    st.json(json_array)
 else:
     st.write("Data not found or could not be loaded.")
