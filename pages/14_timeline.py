e206e2ea288c8b45f91f0339c4fe2131c8363541
NoerFajrin <frinrin23@gmail.com>
2023-11-10 10:36:07 +0700

    kk

--- pages/14_timeline.py
+++ pages/14_timeline.py
@@ -4,21 +4,21 @@ import pandas as pd
 
 # Contoh data untuk timeline
 data = {
-    "Event": ["Perancangan", "Perakitan", "Uji Coba", "Analisis & Kesimpulan"],
-    "Start Date": ["2023-04-01", "2023-05-01", "2023-09-01", "2023-10-01"],
-    "End Date": ["2023-05-01", "2023-09-15", "2023-10-05", "2023-11-01"]
+    "Aktivitas": ["Perancangan", "Perakitan", "Uji Coba", "Analisis & Kesimpulan"],
+    "Tanggal Mulai": ["2023-04-01", "2023-05-01", "2023-09-01", "2023-10-01"],
+    "Tanggal Selesai": ["2023-05-01", "2023-09-15", "2023-10-05", "2023-11-01"]
 }
 
 df = pd.DataFrame(data)
-df['Start Date'] = pd.to_datetime(df['Start Date'])
-df['End Date'] = pd.to_datetime(df['End Date'])
+df['Tanggal Mulai'] = pd.to_datetime(df['Tanggal Mulai'])
+df['Tanggal Selesai'] = pd.to_datetime(df['Tanggal Selesai'])
 
 # Fungsi untuk membuat timeline
 
 
 def plot_timeline(data):
-    fig = px.timeline(data, x_start="Start Date",
-                      x_end="End Date", y="Event", title="Timeline")
+    fig = px.timeline(data, x_start="Tanggal Mulai",
+                      x_end="Tanggal Selesai", y="Aktivitas", title="Timeline")
     return fig
 
 
