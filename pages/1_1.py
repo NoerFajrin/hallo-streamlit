97adb5caec7e38a5400dd4744126d0abbd89725a
NoerFajrin <frinrin23@gmail.com>
2023-11-15 07:24:26 +0700

    x

---
+++ pages/1_1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from PIL import Image
+
+
+st.title("Aviation Activities in Kertajati International Airport ✈️")
+image = Image.open("/aset/desain sistem.png")
+st.image(image)
