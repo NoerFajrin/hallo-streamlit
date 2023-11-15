ea81d2677900169f6f150e29043c6db131a5d6bb
NoerFajrin <frinrin23@gmail.com>
2023-11-15 08:05:37 +0700

    s

--- pages/0_Desain_&_Sistem.py
+++ pages/0_Desain_&_Sistem.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Sistem Desain")
+st.title("- Sistem Desain")
 image1 = Image.open("aset/desain.png")
 st.image(image1)
-
-st.title("Desain Hardware")
+st.write("\n")  # Ini akan menambahkan satu baris kosong
+st.write("\n")  # Ini akan menambahkan satu baris kosong
+st.title("- Desain Hardware")
 image2 = Image.open("aset/desainhw.png")
 st.image(image2)
