ccdcd01ebc3e71ee008f03cfc8a86afc4a3f62c7
NoerFajrin <frinrin23@gmail.com>
2023-11-15 08:00:37 +0700

    ss

--- pages/0_Desain_&_Sistem.py
+++ pages/0_Desain_&_Sistem.py
@@ -5,6 +5,7 @@ from PIL import Image
 st.title("Sistem Desain")
 image = Image.open("aset/desain.png")
 st.image(image)
+
 st.title("Desain Prototipe")
 image = Image.open("aset/desainhw.png")
 st.image(image)
