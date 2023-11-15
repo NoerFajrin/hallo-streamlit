ceb1162c7bdc278e03afb928bcdab346071c988a
NoerFajrin <frinrin23@gmail.com>
2023-11-15 08:25:08 +0700

    d

--- pages/0_Desain_&_Sistem.py
+++ pages/0_Desain_&_Sistem.py
@@ -9,3 +9,18 @@ st.write("\n")  # Ini akan menambahkan satu baris kosong
 st.title("- Desain Hardware")
 image2 = Image.open("aset/desainhw.png")
 st.image(image2)
+st.write("\n")  # Ini akan menambahkan satu baris kosong
+st.write("\n")  # Ini akan menambahkan satu baris kosong
+st.title("- Spesifikasi Timbangan")
+image3 = Image.open("aset/timbangan.png")
+st.image(image3)
+st.write("\n")  # Ini akan menambahkan satu baris kosong
+st.write("\n")  # Ini akan menambahkan satu baris kosong
+st.title("- Spesifikasi Infantometer")
+image4 = Image.open("aset/infan.png")
+st.image(image4)
+st.write("\n")  # Ini akan menambahkan satu baris kosong
+st.write("\n")  # Ini akan menambahkan satu baris kosong
+st.title("- Spesifikasi Pita LiLa")
+image5 = Image.open("aset/pita.png")
+st.image(image5)
